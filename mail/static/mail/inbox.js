994e012b0a3b8450e14155fe20a7485766c7a6f6
agathasta <kalidonia@hotmail.com>
2021-04-08 13:00:58 +0200

    Working grid

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -1,10 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-  // Use buttons to toggle between views
-  document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
-  document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
-  document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
-  document.querySelector('#compose').addEventListener('click', compose_email);
+  // Use buttons to toggle between views (after changing background color of button)
+  document.querySelector('#inbox').addEventListener('click', (event) => {
+    document.querySelectorAll('button').forEach(button => button.classList.remove('active'));
+    event.target.classList.add('active');
+    load_mailbox('inbox');
+  });
+  document.querySelector('#sent').addEventListener('click', (event) => {
+    document.querySelectorAll('button').forEach(button => button.classList.remove('active'));
+    event.target.classList.add('active');
+    load_mailbox('sent');
+  });
+  document.querySelector('#archived').addEventListener('click', (event) => {
+    document.querySelectorAll('button').forEach(button => button.classList.remove('active'));
+    event.target.classList.add('active');
+    load_mailbox('archive');
+  });
+  document.querySelector('#compose').addEventListener('click', (event) => {
+    document.querySelectorAll('button').forEach(button => button.classList.remove('active'));
+    event.target.classList.add('active');
+    compose_email();
+  });
 
   // By default, load the inbox
   load_mailbox('inbox');
@@ -29,5 +45,5 @@ function load_mailbox(mailbox) {
   document.querySelector('#compose-view').style.display = 'none';
 
   // Show the mailbox name
-  document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+  document.querySelector('#emails-view').innerHTML = `<h2>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h2>`;
 }
