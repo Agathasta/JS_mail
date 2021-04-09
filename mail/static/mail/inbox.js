7441998b1a598173108efc7328b6ef3eafde7b13
agathasta <kalidonia@hotmail.com>
2021-04-09 09:07:54 +0200

    Add sidebar and form

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -1,29 +1,38 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 
   // Use buttons to toggle between views (after changing background color of button)
-  document.querySelector('#inbox').addEventListener('click', (event) => {
-    document.querySelectorAll('button').forEach(button => button.classList.remove('active'));
-    event.target.classList.add('active');
-    load_mailbox('inbox');
-  });
-  document.querySelector('#sent').addEventListener('click', (event) => {
-    document.querySelectorAll('button').forEach(button => button.classList.remove('active'));
-    event.target.classList.add('active');
-    load_mailbox('sent');
-  });
-  document.querySelector('#archived').addEventListener('click', (event) => {
-    document.querySelectorAll('button').forEach(button => button.classList.remove('active'));
-    event.target.classList.add('active');
-    load_mailbox('archive');
-  });
-  document.querySelector('#compose').addEventListener('click', (event) => {
-    document.querySelectorAll('button').forEach(button => button.classList.remove('active'));
-    event.target.classList.add('active');
-    compose_email();
-  });
+  document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
+  document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
+  document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
+  document.querySelector('#compose').addEventListener('click', () => compose_email());
 
   // By default, load the inbox
   load_mailbox('inbox');
+
+  // Change background color of buttons when clicked
+  document.querySelectorAll('button').forEach(button => {
+    button.onclick = (event) => {
+      document.querySelectorAll('button').forEach(button => button.classList.remove('active'));
+      event.target.classList.add('active');
+    }
+  });
+
+  // Send an email
+  document.querySelector('#compose-form').onsubmit = () => {
+    fetch('/emails', {
+      method: 'POST',
+      body: JSON.stringify({
+        recipients: document.querySelector('#compose-recipients').value,
+        subject: document.querySelector('#compose-subject').value,
+        body: document.querySelector('#compose-body').value
+      })
+    })
+      .then(response => response.json())
+      .then(result => {
+        console.log(result);
+      });
+    return false;
+  };
 });
 
 function compose_email() {
@@ -36,14 +45,23 @@ function compose_email() {
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
+
+  document.querySelector('#compose-recipients').focus()
 }
 
 function load_mailbox(mailbox) {
-  
+
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h2>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h2>`;
+
+  fetch(`/emails/${mailbox}`)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+    });
+
 }
